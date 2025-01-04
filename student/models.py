49b48c14ea4c5c3f6002fce2249c5dbd35fb9e47
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-04 14:20:51 +0530

    initial commit

---
+++ student/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class student(models.Model):
+    name = models.CharField(max_length=60)
+    address = models.CharField(max_length=150)
+    lattitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    route_id = models.CharField(max_length=10)
+    student_id = models.AutoField()
+
+    def __str__(self):
+        return self.name
